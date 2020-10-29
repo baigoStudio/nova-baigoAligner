0ad65483db9f099a629cd4d1bb1a388f8ce6ec21
黄易锋 <fone@huangyifengdemac-pro.local>
2020-10-29 04:51:11 +0000

    a

--- Scripts/main.js
+++ Scripts/main.js
@@ -62,75 +62,28 @@ nova.commands.register('baigoAligner.removeBeforeColon', (editor) => {
 
 var baigoAligner = {
     align: function(selectedText, symbol, remove){
-        var longest          = 0;
-        var newText          = '';
         var arr_selectedText = selectedText.split('\n');
-        var lastLine         = arr_selectedText[arr_selectedText.length - 1];
 
         if (typeof symbol == 'undefined') {
             symbol = '=';
+        }
+
+        if (symbol == '=') {
+            if (selectedText.indexOf('.=') > 0) {
+                symbol = '.=';
+            } else if (selectedText.indexOf('+=') > 0) {
+                symbol = '+=';
+            }
         }
 
         if (typeof remove == 'undefined') {
             remove = false;
         }
 
-        for (var selectedLine of arr_selectedText) {
-            if (selectedLine.indexOf(symbol) > 0) {
-                var arr_textItem       = selectedLine.split(symbol);
-                var first_selectedLine = arr_textItem[0].trim();
-
-                if (first_selectedLine.length > longest) {
-                    longest = first_selectedLine.length;
-                }
-            }
-        }
-
-        for (var selectedLine of arr_selectedText) {
-            var newLine = '';
-
-            if (selectedLine.indexOf(symbol) > 0) {
-                var arr_textItem       = selectedLine.split(symbol);
-                var first_selectedLine = arr_textItem[0].trim();
-                var lastItem           = arr_textItem[arr_textItem.length - 1];
-                var diff               = longest - first_selectedLine.length;
-
-                arr_textItem[0] = baigoAligner.trim(arr_textItem[0], 'right');
-
-                if (remove !== true) {
-                    if (diff > 0) {
-                        for (var iii = 0; iii < diff; iii++) {
-                            arr_textItem[0] += ' ';
-                        }
-                    }
-                }
-
-                if (symbol === '=') {
-                    arr_textItem[0] += ' ';
-                }
-
-                for (var text of arr_textItem) {
-                    newLine += text;
-
-                    if (text != lastItem) {
-                        newLine += symbol;
-                    }
-                }
-            } else {
-                newLine += selectedLine;
-            }
-
-            newText += newLine;
-
-            if (selectedLine != lastLine) {
-                newText += '\n';
-            }
-        }
-
-        return newText;
+        return baigoAligner.getText(arr_selectedText, symbol, remove);
     },
-    trim: function(string, side){
-        var returnStr = '';
+    getTrim: function(string, side){
+        var str_return = '';
 
         if (typeof side == 'undefined') {
             side = '';
@@ -138,18 +91,88 @@ var baigoAligner = {
 
         switch (side) {
             case 'left':
-                returnStr = string.replace(/(^\s*)/g, '');
+                str_return = string.replace(/(^\s*)/g, '');
             break;
 
             case 'right':
-                returnStr = string.replace(/(\s*$)/g, '');
+                str_return = string.replace(/(\s*$)/g, '');
             break;
 
             default:
-                returnStr = string.trim();
+                str_return = string.trim();
             break;
         }
 
-        return returnStr;
+        return str_return;
+    },
+    getLongest: function(selectedText, symbol) {
+        var num_longest = 0;
+
+        for (var str_selectedLine of selectedText) {
+            if (str_selectedLine.indexOf(symbol) > 0) {
+                var arr_textItem  = str_selectedLine.split(symbol);
+                var str_firstItem = arr_textItem[0].trim();
+
+                if (str_firstItem.length > num_longest) {
+                    num_longest = str_firstItem.length;
+                }
+            }
+        }
+
+        return num_longest;
+    },
+    getText: function(selectedText, symbol, remove) {
+        var str_newText  = '';
+        var num_longest  = baigoAligner.getLongest(selectedText, symbol);
+        var str_lastLine = selectedText[selectedText.length - 1];
+
+        for (var str_selectedLine of selectedText) {
+            var str_newLine = '';
+
+            if (str_selectedLine.indexOf(symbol) > 0) {
+                str_newLine += baigoAligner.getLine(str_selectedLine, symbol, num_longest, remove);
+            } else {
+                str_newLine += str_selectedLine;
+            }
+
+            str_newText += str_newLine;
+
+            if (str_selectedLine != str_lastLine) {
+                str_newText += '\n';
+            }
+        }
+
+        return str_newText;
+    },
+    getLine: function(selectedLine, symbol, longest, remove) {
+        var str_newLine   = '';
+        var arr_textItem  = selectedLine.split(symbol);
+        var str_firstItem = arr_textItem[0].trim();
+        var str_lastItem  = arr_textItem[arr_textItem.length - 1];
+        var num_diff      = longest - str_firstItem.length;
+
+        arr_textItem[0] = baigoAligner.getTrim(arr_textItem[0], 'right');
+
+        if (remove !== true) {
+            if (num_diff > 0) {
+                for (var iii = 0; iii < num_diff; iii++) {
+                    arr_textItem[0] += ' ';
+                }
+            }
+        }
+
+        if (symbol == '=' || symbol == '.=' || symbol == '+=' ) {
+            arr_textItem[0] += ' ';
+        }
+
+        for (var str_text of arr_textItem) {
+            str_newLine += str_text;
+
+            if (str_text != str_lastItem) {
+                str_newLine += symbol;
+            }
+        }
+
+        return str_newLine;
     }
 };
